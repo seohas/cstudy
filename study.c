5da95a9d8cf1c5d78c3e27d5a2205068eba4372b
seohas <77061677+seohas@users.noreply.github.com>
2021-02-03 00:07:17 +0900

    Add files via upload
    A simple example of recursion

---
+++ study.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+	printf("정수를 입력하세요: ");
+	int x = 0;
+	scanf_s("%d", &x);
+	printf("%d", 1+triple(x));
+	return 0;
+}
+int triple(int n)
+{
+	if (n == 1)
+	{
+		return 0;
+	}
+	else
+	{
+		return (pow(n, 3) + triple(n - 1));
+		
+	}
+}
