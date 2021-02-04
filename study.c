6a539bf35f5585a30ecd4668d72782ef548edead
seohas <77061677+seohas@users.noreply.github.com>
2021-02-04 21:26:17 +0900

    Add files via upload

---
+++ study.c
@@ -0,0 +1,33 @@
+﻿#include<stdio.h>
+#include <string.h>
+#define WORDS 6
+int main(void)
+{
+	char dic[WORDS][2][50] = {
+		{"FUBAO","푸바오"},
+		{"BANANA","바나나"},
+		{"PINEAPPLE","파인애플"},
+		{"GRAPE","포도"},
+		{"BLUEBERRY","블루베리"},
+		{"RASPBERRY","라즈베리"}
+	};
+	printf("단어를 입력하십시오: ");
+	char ans[50];
+	scanf_s(" %s", ans, 50);
+	int findflag = 0;
+	char kor[50];
+	for (int i = 0; i < WORDS; i++)
+	{
+		if (strcmp(dic[i][0], ans) == 0)
+		{
+			findflag = 1;
+			strcpy_s(kor, 50,dic[i][1]);
+			printf("%s: %s", ans, kor);
+			return 0;
+		}
+	}
+	
+	printf("없는 단어입니다.");
+	
+	return 0;
+}
