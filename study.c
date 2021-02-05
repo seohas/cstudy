d892b1c69eee7ee89a30ac3a509ff5f44442023e
seohas <77061677+seohas@users.noreply.github.com>
2021-02-06 03:17:38 +0900

    Add files via upload

---
+++ study.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+int main(void)
+{
+	char sentence[100];
+	printf("텍스트를 입력하시오: ");
+	gets_s(sentence, 99);
+	int i, k = 0;
+	for (i = 0; sentence[i] != 0; i++)
+	{
+		if ((sentence[i] != ' ')||(sentence[i+1]!=' '))
+		{
+			sentence[k++] = sentence[i];
+		}
+		
+	}
+	sentence[k] = 0;
+
+	printf("%s", sentence);
+	return 0;
+}
