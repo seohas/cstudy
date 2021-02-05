277c61e396a94252cece41e8ae35b8356cef06e2
seohas <77061677+seohas@users.noreply.github.com>
2021-02-06 02:05:05 +0900

    Add files via upload

---
+++ study.c
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+	char sentence[100] = { '0' };
+	printf("입력 문자열: ");
+	scanf_s("%s", sentence, 100);
+	int count;
+	for (int k = 0; sentence[k] != 0; k++)
+	{
+		for (int j = 0; sentence[j] != 0; j++)
+		{
+			if (sentence[k] < sentence[j])
+			{
+				char tmp;
+				tmp = sentence[k];
+				sentence[k] = sentence[j];
+				sentence[j] = tmp;
+			}
+		}
+	}
+	for (int i = 0; sentence[i] != 0; i++)
+	{
+		count = 0;
+		for (int j = i + 1; sentence[j] != 0; j++)
+		{
+			if (sentence[i] == sentence[j])
+			{
+				count = j - i;
+			}
+		}
+		if (sentence[i-1] != sentence[i])
+		{
+			printf("\n\n%c문자가 %d번 등장하였음! ", sentence[i], count+1);
+		}
+		
+	}
+	
+	return 0;
+}
